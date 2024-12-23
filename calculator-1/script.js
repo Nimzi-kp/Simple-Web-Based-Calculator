642c2eff86cd92774756976660d7040a4242a1a9
NIMZI KP <111420551+Nimzi-kp@users.noreply.github.com>
2024-12-23 09:51:48 +0530

    script.js 1.2

--- calculator-1/script.js
+++ calculator-1/script.js
@@ -1,25 +1,23 @@
-let newCalculation;
+let newcal;
 
 function cliked(val){
-    if(newCalculation){
-        document.getElementById('screen').value=""
-        newCalculation=false
-        if(val=='.'){
-            document.getElementById("screen").value="0"
-        }
+    if(newcal){
+        document.getElementById("screen").value=""
+        newcal=false
     }
     document.getElementById('screen').value+=val
 }
 
 function clearScreen(){
     document.getElementById("screen").value=""
+    newcal=true
 }
 
 function equalCliked(){
     var text=document.getElementById("screen").value
     var result=eval(text)
     document.getElementById("screen").value=result
-    newCalculation=true
+    newcal=true
 }
 function backspace() {
     let display = document.getElementById("screen");
